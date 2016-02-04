240d1a84fef5835c74eb4651d63b5ab5d7a4c286
pranav_tendolkar <ptendolk@us.ibm.com>
2016-02-04 09:39:54 +0530

    added image upload code. untested

--- whatsapp/layer.py
+++ whatsapp/layer.py
@@ -13,6 +13,33 @@ import dialog
 
 
 
+from .cli import Cli, clicmd
+from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.auth import YowAuthenticationProtocolLayer
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.network import YowNetworkLayer
+import sys
+from yowsup.common import YowConstants
+import datetime
+import os
+import logging
+from yowsup.layers.protocol_receipts.protocolentities    import *
+from yowsup.layers.protocol_groups.protocolentities      import *
+from yowsup.layers.protocol_presence.protocolentities    import *
+from yowsup.layers.protocol_messages.protocolentities    import *
+from yowsup.layers.protocol_acks.protocolentities        import *
+from yowsup.layers.protocol_ib.protocolentities          import *
+from yowsup.layers.protocol_iq.protocolentities          import *
+from yowsup.layers.protocol_contacts.protocolentities    import *
+from yowsup.layers.protocol_chatstate.protocolentities   import *
+from yowsup.layers.protocol_privacy.protocolentities     import *
+from yowsup.layers.protocol_media.protocolentities       import *
+from yowsup.layers.protocol_media.mediauploader import MediaUploader
+from yowsup.layers.protocol_profiles.protocolentities    import *
+from yowsup.common.tools import ModuleTools
+
+
+
 
 class EchoLayer(YowInterfaceLayer):
 
@@ -35,6 +62,7 @@ class EchoLayer(YowInterfaceLayer):
                     top5 = send_graph(graph)
                 send_top_performers(top5)
 
+            image_send(messageProtocolEntity.getFrom(), 'iamge.jpg', caption="Hello")
             print output
             #output=message
             outgoingMessageProtocolEntity = TextMessageProtocolEntity(
@@ -48,3 +76,55 @@ class EchoLayer(YowInterfaceLayer):
     def onReceipt(self, entity):
         ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
         self.toLower(ack)
+
+# kaand starts here
+    def image_send(self, number, path, caption=None):
+        if self.assertConnected():
+            jid = self.aliasToJid(number)
+            entity = RequestUploadIqProtocolEntity(RequestUploadIqProtocolEntity.MEDIA_TYPE_IMAGE, filePath=path)
+            successFn = lambda successEntity, originalEntity: self.onRequestUploadResult(jid, path, successEntity, originalEntity, caption)
+            errorFn = lambda errorEntity, originalEntity: self.onRequestUploadError(jid, path, errorEntity, originalEntity)
+
+            self._sendIq(entity, successFn, errorFn)
+
+    def onRequestUploadResult(self, jid, filePath, resultRequestUploadIqProtocolEntity, requestUploadIqProtocolEntity, caption = None):
+        if requestUploadIqProtocolEntity.mediaType == RequestUploadIqProtocolEntity.MEDIA_TYPE_AUDIO:
+            doSendFn = self.doSendAudio
+        else:
+            doSendFn = self.doSendImage
+
+        if resultRequestUploadIqProtocolEntity.isDuplicate():
+            doSendFn(filePath, resultRequestUploadIqProtocolEntity.getUrl(), jid,
+                             resultRequestUploadIqProtocolEntity.getIp(), caption)
+        else:
+            successFn = lambda filePath, jid, url: doSendFn(filePath, url, jid, resultRequestUploadIqProtocolEntity.getIp(), caption)
+            mediaUploader = MediaUploader(jid, self.getOwnJid(), filePath,
+                                      resultRequestUploadIqProtocolEntity.getUrl(),
+                                      resultRequestUploadIqProtocolEntity.getResumeOffset(),
+                                      successFn, self.onUploadError, self.onUploadProgress, async=False)
+            mediaUploader.start()
+
+
+    ########### callbacks ############
+
+    def onRequestUploadError(self, jid, path, errorRequestUploadIqProtocolEntity, requestUploadIqProtocolEntity):
+        print("Request upload for file %s for %s failed" % (path, jid))
+
+    def onUploadError(self, filePath, jid, url):
+        logger.error("Upload file %s to %s for %s failed!" % (filePath, url, jid))
+
+    def onUploadProgress(self, filePath, jid, url, progress):
+        sys.stdout.write("%s => %s, %d%% \r" % (os.path.basename(filePath), jid, progress))
+        sys.stdout.flush()
+
+    def onUploadProgress(self, filePath, jid, url, progress):
+        sys.stdout.write("%s => %s, %d%% \r" % (os.path.basename(filePath), jid, progress))
+        sys.stdout.flush()
+
+    def doSendImage(self, filePath, url, to, ip=None, caption=None):
+        entity = ImageDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to, caption = caption)
+        self.toLower(entity)
+
+    def doSendAudio(self, filePath, url, to, ip=None, caption=None):
+        entity = AudioDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to)
+        self.toLower(entity)
